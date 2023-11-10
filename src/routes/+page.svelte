465cb1afea816f5d2267d5edaaf4f1f36c65a1fd
Carlos Frias <carlos@peerwell.co>
2023-11-10 19:35:26 +0100

    style: page structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,14 +7,26 @@
 	}
 </script>
 
-<div>
-	<h1>Val's Birthday</h1>
+<div class="app">
+	<header><h1>Val's Birthday</h1></header>
 
-	<div>Val is {getAge()} years old</div>
-	<button on:click={celebrate}>Let's celebrate!</button>
+	<main>
+		<div>Val is {getAge()} years old</div>
+		<button on:click={celebrate}>Let's celebrate!</button>
+	</main>
+
+	<footer>
+		<p><a href="https://github.com/terracota-p/val-birthday">source</a></p>
+	</footer>
 </div>
 
 <style>
+	.app {
+		display: flex;
+		flex-direction: column;
+		min-height: 100vh;
+	}
+
 	button {
 		text-align: center;
 		background-color: black;
