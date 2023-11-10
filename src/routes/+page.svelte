09f448ad41f9a4523b665e6c0aadbcae9857159c
Carlos Frias <carlos@peerwell.co>
2023-11-10 18:44:15 +0100

    feat: song

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import song from "./stephen-hawking-happy-birthday-valeria.mp3"
+
+	function celebrate() {
+    const audio = new Audio();
+    audio.src = song;
+		audio.play();
+	}
+</script>
+
+<h1>Val's Birthday</h1>
+
+<div>09/11/2022</div>
+<button on:click={celebrate}>Let's celebrate!</button>
