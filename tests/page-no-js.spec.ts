6e046aedb44d0b51065c401c3113568033b9f355
Carlos Frias <carlos@peerwell.co>
2023-11-29 16:56:08 +0100

    test: without js

---
+++ tests/page-no-js.spec.ts
@@ -0,0 +1,57 @@
+import { expect, test } from '@playwright/test';
+
+test.use({ javaScriptEnabled: false });
+
+test('should celebrate', async ({ page }) => {
+	await page.goto('/');
+
+	await page.getByTestId('celebrate').click();
+
+	await expect(page.getByTestId('key')).toBeVisible();
+});
+
+test('should generate key with empty knowledge', async ({ page }) => {
+	await page.goto('/');
+	await page.getByTestId('celebrate').click();
+
+	await page.getByTestId('generate').click();
+
+	await expect(page.getByTestId('key')).not.toBeEmpty();
+	const knowledgeTextarea = page.getByTestId('knowledge');
+	await expect(knowledgeTextarea).toBeVisible();
+	await expect(knowledgeTextarea).toBeEmpty();
+});
+
+test('should not give knowledge with wrong key', async ({ page }) => {
+	await page.goto('/');
+	await page.getByTestId('celebrate').click();
+	await page.getByTestId('generate').click();
+
+	const keyInput = page.getByTestId('key');
+	await keyInput.fill('wrong-key');
+	await keyInput.press('Enter');
+
+	await expect(page.getByTestId('knowledge')).toBeHidden();
+});
+
+test('should save and retrieve knowledge', async ({ page }) => {
+	await page.goto('/');
+	await page.getByTestId('celebrate').click();
+	await page.getByTestId('generate').click();
+
+	const keyInput = page.getByTestId('key');
+	const generatedKey = await keyInput.inputValue();
+	const knowledge = 'All my knowledge';
+	const knowledgeTextarea = page.getByTestId('knowledge');
+	await knowledgeTextarea.fill(knowledge);
+	await page.getByTestId('save').click();
+
+	await keyInput.fill('wrong-key');
+	await keyInput.press('Enter');
+	await expect(knowledgeTextarea).toBeHidden();
+
+	await keyInput.fill(generatedKey);
+	await keyInput.press('Enter');
+	await expect(knowledgeTextarea).toBeVisible();
+	await expect(knowledgeTextarea).toHaveValue(knowledge);
+});
